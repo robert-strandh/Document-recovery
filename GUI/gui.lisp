4744e76027ab1ab08c1e34137e4fc85f40ecf25f
Robert Strandh <robert.strandh@gmail.com>
2024-07-30 12:17:01 +0200

    Define application frame and commands for starting and quitting.

--- GUI/gui.lisp
+++ GUI/gui.lisp
@@ -1,2 +1,29 @@
 (cl:in-package #:salvager-gui)
 
+(clim:define-application-frame salvager ()
+  ((%current-image :initform nil :accessor current-image))
+  (:panes
+   (image :application :width 1100 :height 4500
+                       :scroll-bars nil
+                       :display-function 'display-image)
+   (interactor :interactor :width 1100 :height 100 :max-height 100
+                           :scroll-bars nil))
+  (:layouts
+   (default (clim:vertically ()
+              (clim:scrolling
+                  (:width 1100 :min-width 1100
+                   :height 1000 :scroll-bars t)
+                image)
+              (clim:scrolling
+                  (:width 100 :max-width 100
+                   :scroll-bars t)
+                interactor)))))
+
+(defun display-image (frame pane)
+  (declare (ignore frame pane)))
+
+(defun salvager ()
+  (clim:run-frame-top-level (clim:make-application-frame 'salvager)))
+
+(define-salvager-command (com-quit :name t) ()
+  (clim:frame-exit clim:*application-frame*))
